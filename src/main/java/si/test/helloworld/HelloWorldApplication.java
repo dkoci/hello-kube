045c2b5c7563bc71d2f38d89011e12b893e87802
Kocbek Davorin <dkocbek@ts.telekom.si>
2020-09-30 13:23:10 +0200

    First commit

---
+++ src/main/java/si/test/helloworld/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package si.test.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HelloWorldApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelloWorldApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
